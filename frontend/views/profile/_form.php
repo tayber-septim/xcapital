e2c0063ee6d33b339c2bc8a6c154ab9e70f0bac6
Evalon <evalonnn@gmail.com>
2017-08-11 16:14:07 +0300

    fix profile information change

--- frontend/views/profile/_form.php
+++ frontend/views/profile/_form.php
@@ -3,33 +3,32 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
-/* @var $this yii\web\View */
 /* @var $model frontend\models\profile\ProfileUserModel */
 /* @var $form yii\widgets\ActiveForm */
 ?>
 
-<div class="profile-user-model-form">
+
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'username')->textInput(['maxlength' => true]) ?>
+       <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'username')->textInput(['maxlength' => true])->label(false) ?>
 
-    <?= $form->field($model, 'perfectMoney')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'email')->textInput(['maxlength' => true])->label(false) ?>
 
-    <?= $form->field($model, 'peyeer')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'perfectMoney')->textInput(['maxlength' => true])->label(false) ?>
 
-    <?= $form->field($model, 'bitcoin')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'peyeer')->textInput(['maxlength' => true])->label(false) ?>
 
-    <?= $form->field($model, 'qiwi')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'bitcoin')->textInput(['maxlength' => true])->label(false) ?>
 
-    <?= $form->field($model, 'yandex')->textInput(['maxlength' => true]) ?>
+        <?= $form->field($model, 'qiwi')->textInput(['maxlength' => true])->label(false) ?>
 
-    <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
-    </div>
+        <?= $form->field($model, 'yandex')->textInput(['maxlength' => true])->label(false) ?>
 
+        <div class="form-group">
+            <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        </div>
+  
     <?php ActiveForm::end(); ?>
-
-</div>
--- frontend/views/profile/update.php
+++ frontend/views/profile/update.php
@@ -3,12 +3,38 @@
 use yii\helpers\Html;
 use yii\helpers\Url;
 
-/* @var $this yii\web\View */
+
 /* @var $model frontend\models\profile\ProfileUserModel */
 // var_dump($model->id);exit();
 
+include "components/head_profile.php";
+?>
+
+
+<?php $this->beginBody() ?>
+
+<div class="section_name">
+    <div class="adm-containter">
+      <h2><?= Yii::$app->user->identity->username ?></h2>
+    <div class="login">
+       <?= Html::a('Logout', ['site/logout'], ['data' => ['method' => 'post']]) ?>
+      </div>
+    </div> 
+</div>
+
+<div class="all_admin">
+ <!-- Подключаем левое меню навигации  -->
+
+
+<section>
+<div class="adm-containter section-content">
+  <div class="personal">
+
+<?php
+
 if($model->id == Yii::$app->user->identity->id){
-	$this->title = 'Update Profile User Model: ' . $model->username;
+
+	$this->title = 'Update Profile : ' . $model->username;
 	$this->params['breadcrumbs'][] = ['label' => 'Profile User Models', 'url' => ['index']];
 	$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
 	$this->params['breadcrumbs'][] = 'Update';
@@ -21,6 +47,28 @@ if($model->id == Yii::$app->user->identity->id){
 			'model' => $model,
 		]) ?>
 	</div>
+
 	<?php }else{
+
 		return Yii::$app->response->redirect(Url::to('/profile'));
 	}?>
+
+
+  </div>
+</div>
+</section>
+
+
+</div>
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+
+        <p class="pull-right"><?= Yii::powered() ?></p>
+    </div>
+</footer>
+
+<?php $this->endBody() ?>
+</body>
+</html>
+<?php $this->endPage() ?>
